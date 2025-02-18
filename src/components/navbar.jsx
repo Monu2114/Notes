d64ad39da6ba1e7bc89a1f811a2ffbf975bbe6c9
Monu_2114 <monu.nanabala@gmail.com>
2025-02-18 13:47:07 +0530

    avatar in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,10 +2,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 export default function navbar({ username }) {
   return (
     <div className="h-5/6 w-1/4 rounded-xl border-gray-300 border">
-      <div className="flex flex-col gap-64 ml-2">
+      <div className="flex flex-col gap-56 ml-2">
         <div className="flex flex-col gap-2">
           <div className="flex gap-2  mt-2 p-1 border-b">
             <div>
@@ -28,11 +30,15 @@ export default function navbar({ username }) {
         </div>
 
         <div className="flex ml-2 gap-2">
-          <div>
-            <Image src="/note.png" height={80} width={20} alt="Notes" />
+          <div className="">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
           </div>
-
-          <p>{username}</p>
+          <div>
+            <p>{username}</p>
+          </div>
         </div>
       </div>
     </div>
